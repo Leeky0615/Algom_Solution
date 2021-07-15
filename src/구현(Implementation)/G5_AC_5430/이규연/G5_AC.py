ba617f7d19dbd7bbe08a1cd70b34dc349b5c42a5
Leeky <rbdus7174@naver.com>
2021-07-15 14:42:35 +0900

    0715-[이규연]5430번 AC

---
+++ src/구현(Implementation)/G5_AC_5430/이규연/G5_AC.py
@@ -0,0 +1,25 @@
+for _ in range(int(input())):
+    p = input()
+    s = int(input())
+    arr = input()[1:-1].split(',')
+    if s == 0:
+        arr = []
+    rStatus = False
+    start = 0
+    end = 0
+    for i in p:
+        if i == 'R':
+            rStatus = not rStatus
+        elif i == 'D' and rStatus:
+            end += 1
+        elif i == 'D' and not rStatus:
+            start += 1
+
+    if start + end <= s:
+        result = arr[start:s - end]
+        if rStatus:
+            print('[' + ','.join(result[::-1]) + ']')
+        else:
+            print('[' + ','.join(result) + ']')
+    else:
+        print("error")
