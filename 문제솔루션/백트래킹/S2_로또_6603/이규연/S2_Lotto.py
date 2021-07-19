7afff896b2b1f69aefced195cea93a7155b4a04c
Leeky <rbdus7174@naver.com>
2021-07-19 15:08:08 +0900

    0719_로또_이규연

---
+++ 문제솔루션/백트래킹/S2_로또_6603/이규연/S2_Lotto.py
@@ -0,0 +1,13 @@
+from itertools import combinations
+
+while True:
+    inputs = list(map(int, input().split()))
+    k = inputs[0]
+    if k == 0:
+        break
+    s = inputs[1:]
+    for i in combinations(s, 6):
+        for j in i:
+            print(j, end=' ')
+        print()
+    print()
