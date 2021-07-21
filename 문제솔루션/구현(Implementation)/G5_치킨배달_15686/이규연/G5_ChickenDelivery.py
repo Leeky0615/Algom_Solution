5de6b36529c0998e8feaa136106eee75fa50177b
Leeky <rbdus7174@naver.com>
2021-07-21 20:42:16 +0900

    0721_치킨배달_이규연

--- 문제솔루션/구현(Implementation)/G5_치킨배달_15686/이규연/G5_ChickenDelivery.py
+++ 문제솔루션/구현(Implementation)/G5_치킨배달_15686/이규연/G5_ChickenDelivery.py
@@ -20,7 +20,7 @@ for c in combinations(chicken, m):  # 전체 치킨집 개수에서 남겨야하
         '''
             집 <-> 치킨집 거리가 제일 짧은것을 distance에 더해줘야함.
             이때, 치킨집의 좌표는 위의 조합을 통해 뽑힌 m개의 치킨집을
-            모두 확인.
+            모두 확인
         '''
         distance += min([abs(h[0] - i[0]) + abs(h[1] - i[1]) for i in c])
     result = min(result, distance)  # 결과값은 반복문(조합을 통한 모든 값)이 반복될 때마다 더 작은값으로 변경
