1cc02bc211653502502d22c97247bf8a189a2300
jelly-boop <70804578+jelly-boop@users.noreply.github.com>
2021-07-23 22:08:08 +0900

    0723_홍이

--- 문제솔루션/큐/S3_프린터큐_1966/홍은서/S3_프린터큐.java
+++ 문제솔루션/큐/S3_프린터큐_1966/홍은서/S3_프린터큐.java
@@ -4,55 +4,60 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class S3_프린터큐 {
+public class S3_프린터큐{
     public static void main(String[] args) throws IOException {
-        PriorityQueue<Printer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        ArrayList<Integer> al = new ArrayList<>();
+        PriorityQueue<Printer> queue = new PriorityQueue<>();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int tests = Integer.parseInt(st.nextToken());
 
-        while (tests-- > 0) { // 테스트 단위
-            StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-            int N = Integer.parseInt(st1.nextToken()); // 문서 수
-            int idx = Integer.parseInt(st1.nextToken()); // 인덱스
-            int i=0;
-            int answer=0;
+        ArrayList <Integer> al = new ArrayList<>();
 
+        while (tests --> 0){
             StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
-            while (st2.hasMoreTokens()){
-                int P = Integer.parseInt(st2.nextToken());
-                queue.offer(new Printer(P,i));
+            int papers = Integer.parseInt(st2.nextToken()); // 문서 수
+            int idx = Integer.parseInt(st2.nextToken()); // 현재 위치
+            int i=0;
+
+            StringTokenizer st3 = new StringTokenizer(br.readLine()," "); // 중요도 배열
+
+            while (st3.hasMoreTokens()){ // 중요도에 맞춰서 큐에 넣음
+                int priority = Integer.parseInt(st3.nextToken());
+                queue.add(new Printer(i,priority));
                 i++;
             }
+
+            int k=1;
             while (!queue.isEmpty()){
-                if(queue.poll().i == idx){
-                    answer++;
+                if(queue.poll().position == idx){
+                    al.add(k);
                     break;
-                }else{
-                    answer++;
                 }
+                k++;
             }
-            System.out.println(answer);
-            queue.clear();
+
+
         }
+        for(Integer i : al ){
+            System.out.println(i);
+        }
+
     }
 
-    private static class Printer implements Comparable<Printer> {
-        int p;
-        int i;
-        public Printer(int p, int i) {
-            this.p=p;
-            this.i=i;
-        }
-        @Override
-        public int compareTo(Printer o) {
-            return o.p-this.p;
-        }
+}
+class Printer implements Comparable<Printer>{
+    public int position;
+    public int p;
+    public Printer(int i, int priority) {
+        this.position=i;
+        this.p=priority;
+    }
+    @Override
+    public int compareTo(Printer o) {
+        return o.p-this.p;
     }
 }
