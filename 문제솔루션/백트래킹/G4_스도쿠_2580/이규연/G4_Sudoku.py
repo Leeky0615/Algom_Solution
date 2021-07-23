9a272e542ac1e7d228d145e120afb6073fb05186
Leeky <rbdus7174@naver.com>
2021-07-23 19:13:17 +0900

    0723_스도쿠_이규연

--- 문제솔루션/백트래킹/G4_스도쿠_2580/이규연/G4_Sudoku.py
+++ 문제솔루션/백트래킹/G4_스도쿠_2580/이규연/G4_Sudoku.py
@@ -1,26 +1,49 @@
-# from collections import deque
-#
-g = [list(map(int, input().split())) for _ in range(9)]
-verticals = [[0] * 9 for _ in range(9)]
-box = [[0] * 9 for _ in range(9)]
+g = []
+blank = []
 for i in range(9):
+    temp = list(map(int, input().split()))
     for j in range(9):
-        box[i][j] = g[i//3][j//3]
-        verticals[i][j] = g[j][i]
+        if temp[j] == 0:
+            blank.append((i, j))
+    g.append(temp)
 
 
-print(g)
-print("-----------")
-print(verticals)
-# blank = deque()
-# for i in range(9):
-#     for j in range(9):
-#         if g[i][j] == 0:
-#             blank.append(i, j)
-#
-# while blank:
-#     x, y = blank.popleft()
-#     for i in range(9):
-#         if g[x].count(i) != 1 and :
+def checkAvailableNum(i, j):
+    check = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    for k in range(9):
+        if g[i][k] in check:  # 가로줄 확인
+            check.remove(g[i][k])  # 해당 좌표의 값을 체크리스트에서 제거
+        if g[k][j] in check:  # 세로줄 확인
+            check.remove(g[k][j])  # 해당 좌표의 값을 체크리스트에서 제거
+    i //= 3
+    j //= 3
+    for x in range(i * 3, (i + 1) * 3):
+        for y in range(j * 3, (j + 1) * 3):
+            if g[x][y] in check:
+                check.remove(g[x][y])
 
-# 가로,세로, 3x3 체크 했을 때 빈칸이 하나면 바로 넣기..
+    return check
+
+
+status = False
+
+
+def dfs(x):
+    global status
+    if status:
+        return
+    if x == len(blank):
+        for r in g:
+            print(*r)
+        status = True
+        return
+    else:
+        (i, j) = blank[x]
+        availableNum = checkAvailableNum(i, j)
+
+        for n in availableNum:
+            g[i][j] = n
+            dfs(x + 1)
+            g[i][j] = 0
+
+dfs(0)
