21ed903d4fcab276d8b9d352e19cba9c2070bfec
jelly-boop <70804578+jelly-boop@users.noreply.github.com>
2021-07-29 22:04:22 +0900

    0729_홍이

---
+++ 문제솔루션/DFS와BFS/S1_케빈베이컨의6단계법칙_1389/홍은서/케빈베이컨의6단계법칙.java
@@ -0,0 +1,12 @@
+package DFS와BFS.S1_케빈베이컨의6단계법칙_1389.홍은서;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 케빈베이컨의6단계법칙 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            // 낼 해야지..... 넘 졸려잉
+    }
+
+}
