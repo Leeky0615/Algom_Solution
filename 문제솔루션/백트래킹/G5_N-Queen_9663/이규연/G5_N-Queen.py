580f2c24b1ac989ed71b47fbe7e374c629c244db
Leeky <rbdus7174@naver.com>
2021-09-08 21:15:48 +0900

    0908_small change_이규연

--- 문제솔루션/백트래킹/G5_N-Queen_9663/이규연/G5_N-Queen.py
+++ 문제솔루션/백트래킹/G5_N-Queen_9663/이규연/G5_N-Queen.py
@@ -1,25 +1,28 @@
 n = int(input())
-rd = set()
-ru = set()
+rd = set() # 오른쪽 아래 대각선 -> col - rowNum의 값이 똑같음
+ru = set() # 오른쪽 위 대각선 -> col + rowNum의 값이 똑같음
 visit = [False] * n
 
 result = 0
-
 
 def nqueen(rowNum):
     global rd, ru, visit, result
     if rowNum == n:
         result += 1
         return
-    for col in range(n):
+    for col in range(n):  # 모든 열 반복
+        # 퀸이 놓여 있거나, 오른쪽 아래 대각선 이거나, 오른쪽 위 대각선인 경우가 아닐때만
         if not (visit[col] or (col - rowNum) in rd or (col + rowNum) in ru):
+            # 퀸을 놓고 대각선 경로를 추가한다.
             visit[col] = True
             rd.add(col - rowNum)
             ru.add(col + rowNum)
+            # 다음 행 실행
             nqueen(rowNum + 1)
+            # 백트래킹
             visit[col] = False
-            rd.discard(col-rowNum)
-            ru.discard(col+rowNum)
+            rd.discard(col - rowNum)
+            ru.discard(col + rowNum)
 
 nqueen(0)
 print(result)
